dc91c57c3c3c7af71d8be347b312648df46ea672
Daniel Caccia <danielcaccia@icloud.com>
2025-04-08 01:31:02 +0200

    Update StockQuote service

--- app/finance/stock_quote.py
+++ app/finance/stock_quote.py
@@ -9,6 +9,7 @@ class StockQuote:
             symbol = symbol.upper()
             stock = yf.Ticker(symbol)
             data = stock.history(period="1d")
+            display_name = stock.info.get("displayName")
 
             if data.empty:
                 return {"error": "Invalid stock symbol or no data available."}
@@ -16,6 +17,7 @@ class StockQuote:
             latest_quote = data.iloc[-1]
             return {
                 "symbol": symbol,
+                "display_name": display_name,
                 "date": str(latest_quote.name.date()),
                 "open": latest_quote["Open"],
                 "high": latest_quote["High"],
@@ -32,6 +34,7 @@ class StockQuote:
             symbol = symbol.upper()
             stock = yf.Ticker(symbol)
             data = stock.history(period=period)
+            display_name = stock.info.get("displayName")
 
             if data.empty:
                 return {"error": "Invalid stock symbol or no data available."}
@@ -48,7 +51,11 @@ class StockQuote:
                 for index, row in data.iterrows()
             ]
 
-            return {"symbol": symbol, "history": history}
+            return {
+                "symbol": symbol,
+                "display_name": display_name,
+                "history": history
+            }
 
         except Exception as e:
             return {"error": str(e)}
